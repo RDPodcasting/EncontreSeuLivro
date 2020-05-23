02cfd65b47fddc74cca11d508e48207c042894e4
Thiago de Melo Lima <Thiago_badboy300@hotmail.com>
2020-05-23 09:54:12 -0300

    Initial commit

---
+++ Controllers/LivrosController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EncontreSeuLivro.Controllers
+{
+    public class LivrosController : Controller
+    {
+        public IActionResult Busca()
+        {
+            return View();
+        }
+
+        public IActionResult Lancamentos()
+        {
+            return View();
+        }
+    }
+}
